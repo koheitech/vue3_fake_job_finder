b649676f527bc63d0daae895f5022ae64b676fb2
koheitech <koheitechigh@gmail.com>
2023-06-30 11:38:04 +0300

    pass order term as props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
+    <button @click="handleClick('title')">order by title</button>
+    <button @click="handleClick('salary')">order by salary</button>
+    <button @click="handleClick('location')">order by location</button>
+    <JobList :jobs="jobs" :order="order"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from 'vue';
+import JobList from './components/JobsList.vue'
 import Job from  './types/Job'
+import OrderTerm from  './types/OrderTerm'
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
       { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
@@ -20,7 +25,13 @@ export default defineComponent({
       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
     ])
 
-    return { jobs }
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
   },
   methods: {
   }
